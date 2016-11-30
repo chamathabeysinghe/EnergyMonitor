f67f2b90e180c71e56de9304b61525727d496f6c
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2016-11-30 19:23:15 +0530

    initial commit

---
+++ src/com/ceb/WebController.java
@@ -0,0 +1,40 @@
+package com.ceb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ceb.models.User;
+
+import org.springframework.ui.ModelMap;
+
+//@Controller
+//@RequestMapping("/login")
+//public class WebController{
+// 
+//   @RequestMapping(method = RequestMethod.GET)
+//   public String printHello(ModelMap model) {
+//      return "login";
+//   }
+//
+//}
+
+
+@Controller
+public class WebController {
+
+   @RequestMapping(value = "/login", method = RequestMethod.GET)
+   public ModelAndView user() {
+      return new ModelAndView("login", "command", new User());
+   }
+   
+   @RequestMapping(value = "/postLogin", method = RequestMethod.POST)
+   public String logUser(@ModelAttribute("SpringWeb")User user,ModelMap model) {
+      model.addAttribute("name", user.getName());
+      
+      
+      return "dashboard";
+   }
+}
