7fcf126d89cbec4773eeed40ec56157f134fc98c
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2016-12-17 09:20:58 +0530

    Controllers changed. All working

--- WebContent/WEB-INF/jsp/electricConsumption.jsp
+++ WebContent/WEB-INF/jsp/electricConsumption.jsp
@@ -107,6 +107,7 @@
 	    	</div>
 	    	
 	    	<br><br><br>
+	    	<!-- For the provisional view interfaces -->
 	    	<div class="row " id="provisionalView">
 	    		<div class="col-md-6">
 	    	
@@ -129,6 +130,29 @@
 			         
 			        
 				</div>   
+				
+				<div class="col-md-6">
+	    	
+		    		<div class="box box-info">
+			            <div class="box-header with-border">
+			              <h3 class="box-title">Expected Growth</h3>
+			
+			              <div class="box-tools pull-right">
+			                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			                </button>
+			                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+			              </div>
+			            </div>
+			            <div class="box-body">
+			              <div class="chart">
+			                <canvas id="expectedGrowthChart" style="height: 200px; width: 493px;" height="200" width="554"></canvas>
+			              </div>
+			            </div>
+			          </div>
+			         
+			        
+				</div>   
+				
 				<div class="col-md-3">
 					 <div class="box box-danger">
 			            <div class="box-header with-border">
--- src/com/ceb/WebController.java
+++ src/com/ceb/WebController.java
@@ -50,52 +50,6 @@ public class WebController {
       return "dashboard";
    }
    
-   @RequestMapping(value="/usage",method=RequestMethod.GET)
-   public String consumerUsage(ModelMap model){
-	   String results[]=Bill.getUsageRepor(1,2016);
-	   model.addAttribute("usageList",results);
-	   return "consumerUsage";
-   }
-   
-
-   @RequestMapping(value="/ajaxBillYearChange",method=RequestMethod.POST,produces = "plain/text")
-   @ResponseBody
-   public String ajaxBillYearChange(@RequestBody String year){
-	   int intYear=Integer.parseInt(year);
-	   String results[]=Bill.getUsageRepor(1,intYear);
-	   return results[1];
-   }
-   
-      
-   
-   @RequestMapping(value="/consumption",method=RequestMethod.GET)
-   public String locationUsage(ModelMap model){
-	   
-	   String totlaUsageByYear[]=EnergyConsumption.EnergyConsumptionDAO.getEnergyConsumptionRecordByYear();
-	   model.addAttribute("usageList",totlaUsageByYear);
-	   
-	   String totlaUsageByTime[]=EnergyConsumption.EnergyConsumptionDAO.getCountryEnergyConsumptionCategorizedByTime();
-	   model.addAttribute("usageListByTime",totlaUsageByTime);
-	   
-	   String totlaUsageByProvince[]=EnergyConsumption.EnergyConsumptionDAO.getCountryEnergyConsumptionCategorizedByProvince();
-	   model.addAttribute("usageListByProvince",totlaUsageByProvince);
-	   return "electricConsumption";
-   }
-   
-   @RequestMapping(value="/ajaxConsumptionForProvince",method=RequestMethod.POST,produces = "plain/text")
-   @ResponseBody
-   public String ajaxConsumptionForProvince(@RequestBody String province){
-	   HashMap<String,String> resultsForTotalUsage=EnergyConsumption.EnergyConsumptionDAO.getEnergyConsumptionRecordByYearForProvince(province);
-	   HashMap<String,String> resultsForUsageByTime=EnergyConsumption.EnergyConsumptionDAO.getCountryEnergyConsumptionCategorizedByTimeForProvince(province);
-	   HashMap<String,HashMap<String,String>> l=new HashMap<String,HashMap<String,String>>();
-	   l.put("TotalUsage", resultsForTotalUsage);
-	   l.put("TimeUsage", resultsForUsageByTime);
-	   
-       Gson gson = new Gson();
-       String json = gson.toJson(l);
-
-	   return json;
-   }
    
 
    
