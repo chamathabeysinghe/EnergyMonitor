ee2dd558f8dd27a182d497c9064eee7fcd91f43e
Thulana <thulanakannangara@gmail.com>
2016-12-21 19:14:08 +0530

    sign up working

--- src/com/ceb/database/UserRowMapper.java
+++ src/com/ceb/database/UserRowMapper.java
@@ -9,13 +9,11 @@ public class UserRowMapper implements RowMapper<User>{
 	
 	public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 		User u =new User();
-		
+		u.setId(Integer.parseInt(rs.getString("id")));
+		u.setUserType(rs.getString("userType"));
 		u.setFirstName(rs.getString("firstName"));
 		u.setLastName(rs.getString("lastName"));
-		u.setAddress(rs.getString("address"));
-		u.setNameWithInitials(rs.getString("nameWithInitials"));
 		u.setNIC(rs.getString("NIC"));
-		u.setAddress(rs.getString("address"));
 		u.setPhoneNumber(rs.getString("phoneNumber"));
 		u.setEmail(rs.getString("email"));
 		u.setPassword(rs.getString("password"));
